9db4b7e80a403c689f630acd58162d5b09702c0d
ADIB FARHAN <adib_farhan96@yahoo.com>
2017-03-14 21:14:17 +0800

    Create aaah.go

---
+++ kattis-go/aaah.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var str1 string
+	var str2 string
+
+	fmt.Scanln(&str1)
+	fmt.Scanln(&str2)
+	if len(str1) < len(str2){
+		fmt.Println("no")
+	}else {
+		fmt.Println("go")
+	}
+}
