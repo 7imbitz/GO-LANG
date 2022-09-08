817b79957b1b5d9fcbc3287e1f2c7d92d3c9ea9f
ADIB FARHAN <adibfarhan9642@gmail.com>
2022-09-08 12:02:01 +0800

    Update aaah.go

--- src/Aaah!/aaah.go
+++ src/Aaah!/aaah.go
@@ -1,17 +1,16 @@
-package main
+package main 
 
 import "fmt"
 
-func main(){
-	var str1 string
-	var str2 string
-
-	fmt.Scanln(&str1)
-	fmt.Scanln(&str2)
-	//str compare
-	if len(str1) < len(str2){
-		fmt.Println("no")
-	}else {
-		fmt.Println("go")
-	}
+func main() {
+    var str1,str2 string
+    
+    fmt.Scan(&str1)
+    fmt.Scan(&str2)
+    
+    if (len(str1) < len(str2)) {
+        fmt.Println("no")
+    } else {
+        fmt.Println("go")
+    }
 }
