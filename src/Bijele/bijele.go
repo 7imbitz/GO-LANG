e070f000888a88b3f68e1e2c16fcb49454ff5f7c
ADIB FARHAN <adibfarhan9642@gmail.com>
2022-09-08 11:58:31 +0800

    Update bijele.go

--- src/Bijele/bijele.go
+++ src/Bijele/bijele.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
-	var arr [6]int
-	cor := [6]int{ 1,1,2,2,2,8, }
-
-	for i := 0; i < 6; i++{
-		fmt.Scan(&arr[i])
-		fmt.Print(cor[i] - arr[i], " ")
-	}
+func main() {
+    var arr [6]int
+    cor := [6]int{1,1,2,2,2,8}
+    
+    for i := 0; i < 6; i++{
+        fmt.Scan(&arr[i])
+        fmt.Print(cor[i] - arr[i]," ")
+    }
 }
